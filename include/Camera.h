0f52e024d2fc64bd6e02cc7f550a30a6b4900ec0
rasmussvala <rassv453@student.liu.se>
2023-09-12 10:59:51 +0200

    Update Camera.h

--- include/Camera.h
+++ include/Camera.h
@@ -13,7 +13,8 @@ public:
 	int GetHeight(); // Get the height of the camera
 	glm::vec4 GetLocation(int x, int y); // Get the location of the camera
 	glm::vec4 SetLocation(int x, int y, int z); // Set the location of the camera
-	void saveImage(std::string filename, int width, int height, const std::vector<std::vector<ColorRGB>>& pixels); // Save the image to a file (PPM-format)
+	void saveImage(std::string filename, int width, int height, const std::vector<std::vector<ColorRGB>>& pixels); // Saves the generated image to a file (PPM-format)
+
 	std::vector<std::vector<ColorRGB>> pixels;
 
 private:
