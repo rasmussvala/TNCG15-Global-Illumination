e130bbea14786019161dafcd3605dc9f8a07f97d
molbe532 <molbe532@student.liu.se>
2023-09-12 15:56:33 +0200

    Logg tillagd i Main
    Lagt till en logg som skriver ut varje iteration i for-loopen för att enklare kunna se att den arbetar genom alla pixlar på skärmen.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,45 @@
 #include "../include/ColorRGB.h"
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 int main() {
 	Camera camera{ 600, 600 };
 
-	for (int x = 0; x < camera.GetWidth(); x++) {
-		for (int y = 0; y < camera.GetHeight(); y++) {
-			camera.pixels[x][y] = ColorRGB(1.0, 0.0, 0.0);
+	// öppna textfil för skrivning av linjer skannade
+	std::ofstream logFile("../log.txt"); 
+
+	for (int i = 0; i < camera.GetWidth(); i++) {	
+
+		// För debugging enbart
+		if (logFile.is_open()){
+			std::clog.rdbuf(logFile.rdbuf());
+			std::clog << "\rScanlines remaining: " << (camera.GetHeight() - i) << ' ' << std::flush; 
+			
+		} else {
+        	std::cerr << "Kunde inte öppna loggfilen." << std::endl;
+    	}
+		// ---------------------
+
+		for (int j = 0; j < camera.GetHeight(); j++) {
+			auto r = double(i) / (camera.GetWidth()-1);
+            auto g = double(j) / (camera.GetHeight()-1);
+            auto b = 0;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			camera.pixels[i][j] = ColorRGB(ir, ig, ib);
+
 		}
 	}
 
-	camera.saveImage("C:/Users/Rasmus/Desktop/Skola/MT4B/Advanced Global Illumination and Rendering - TNCG15/RayTracing-TNGC15/images/test.ppm",
+	// För debugging enbart
+	std::clog << "\rDone.                 \n";
+	logFile.close();
+	//---------------------
+
+	camera.saveImage("../test.ppm",
 		camera.GetWidth(), camera.GetHeight(), camera.pixels);
 }
