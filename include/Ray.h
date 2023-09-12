8aaef008f00226c2fd433739121fd1c3bad7184d
rasmussvala <rassv453@student.liu.se>
2023-09-12 11:26:38 +0200

    påbörjad ray och polygon

--- include/Ray.h
+++ include/Ray.h
@@ -1,7 +1,16 @@
 #pragma once
+#include "../include/ColorRGB.h"
+#include "../include/glm/glm.hpp"
+#include "../include/Polygon.h"
 
 class Ray {
 public:
-	// Implementera
-};
+	glm::vec4 start_vertex;
+	glm::vec4 end_vertex; // Optional
+	glm::vec4 direction;
+	Polygon* surface; // Pointer to the surface
+	ColorRGB color;
 
+	Ray(const glm::vec4& start, const glm::vec4& dir, Polygon* surf, const ColorRGB& col)
+		: start_vertex(start), direction(dir), surface(surf), color(col) {}
+};
