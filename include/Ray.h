2417080f7683a6e38d8079001ab5a10e41c20cae
rasmussvala <rassv453@student.liu.se>
2023-09-13 14:15:42 +0200

    flyttat uv-mappning + påbörjat ray
    har även gjort andra ändringar för bättre kontinuitet

--- include/Ray.h
+++ include/Ray.h
@@ -1,7 +1,17 @@
 #pragma once
+#include "../include/glm/glm.hpp"
 
 class Ray {
 public:
-	// Implementera
-};
+	Ray(const glm::vec4& orig, const glm::vec4& dir) : origin(orig), direction(dir) {}
 
+	glm::vec4 getOrigin() const { return origin; }
+	glm::vec4 getDirection() const { return direction; }
+
+	glm::vec4 at(float t) const {
+		return origin + t * direction;
+	}
+private:
+	glm::vec4 origin;
+	glm::vec4 direction;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -8,40 +8,6 @@
 int main() {
 	Camera camera{ 600, 600 };
 
-	// öppna textfil för skrivning av linjer skannade
-	std::ofstream logFile("../log.txt"); 
-
-	for (int i = 0; i < camera.GetWidth(); i++) {	
-
-		// För debugging enbart
-		if (logFile.is_open()){
-			std::clog.rdbuf(logFile.rdbuf());
-			std::clog << "\rScanlines remaining: " << (camera.GetHeight() - i) << ' ' << std::flush; 
-			
-		} else {
-        	std::cerr << "Kunde inte öppna loggfilen." << std::endl;
-    	}
-		// ---------------------
-
-		for (int j = 0; j < camera.GetHeight(); j++) {
-			auto r = double(i) / (camera.GetWidth()-1);
-            auto g = double(j) / (camera.GetHeight()-1);
-            auto b = 0;
-
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			camera.pixels[i][j] = ColorRGB(ir, ig, ib);
-
-		}
-	}
-
-	// För debugging enbart
-	std::clog << "\rDone.                 \n";
-	logFile.close();
-	//---------------------
-
-	camera.saveImage("../test.ppm",
-		camera.GetWidth(), camera.GetHeight(), camera.pixels);
+	camera.saveImage("../images/test.ppm",
+		camera.getWidth(), camera.getHeight(), camera.pixels);
 }
