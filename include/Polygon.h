9abe5024d8f819072b75e32173e1815fa2ab8600
rasmussvala <rassv453@student.liu.se>
2023-09-13 14:59:35 +0200

    påbörjat rectangle och triangle

--- include/Polygon.h
+++ include/Polygon.h
@@ -1,18 +1,40 @@
 #pragma once
+#include "../include/glm/glm.hpp"
+#include "../include/Ray.h"
 
 class Polygon {
 public:
-	// CHATGPT, Denna funktion ska overrideas i Rectangle och Triangle
-	// virtual bool intersect(const Ray& ray, glm::dvec4& intersection_point) const = 0;
+	virtual bool intersect(const Ray& ray, glm::vec4& intersectionPoint) const = 0;
 };
 
 class Rectangle : public Polygon {
 public:
-	// Implement
+	Rectangle(const glm::vec4& p1, const glm::vec4& p2, const glm::vec4& p3, const glm::vec4& p4)
+		: vertex1(p1), vertex2(p2), vertex3(p3), vertex4(p4) {}
+
+	bool intersect(const Ray& ray, glm::vec4& intersectionPoint) const override {
+		// Implement
+	}
+
+private:
+	glm::vec4 vertex1;
+	glm::vec4 vertex2;
+	glm::vec4 vertex3;
+	glm::vec4 vertex4;
 };
 
 class Triangle : public Polygon {
 public:
-	// Implement
+	Triangle(const glm::vec4& p1, const glm::vec4& p2, const glm::vec4& p3)
+		: vertex1(p1), vertex2(p2), vertex3(p3) {}
+
+	bool intersect(const Ray& ray, glm::vec4& intersectionPoint) const override {
+		// Implement
+	}
+
+private:
+	glm::vec4 vertex1;
+	glm::vec4 vertex2;
+	glm::vec4 vertex3;
 };
 
