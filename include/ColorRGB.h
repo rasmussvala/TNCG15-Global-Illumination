68ff8703b387a338e2326676b2fb55281ec69b59
rasmussvala <rassv453@student.liu.se>
2023-10-26 11:19:45 +0200

    separerat på colorRGB

--- include/ColorRGB.h
+++ include/ColorRGB.h
@@ -4,54 +4,23 @@ class ColorRGB {
 public:
     double r, g, b;
 
-    ColorRGB(double red, double green, double blue)
-        : r(red), g(green), b(blue) {}
+    ColorRGB(double red, double green, double blue);
+    ColorRGB();
 
-    ColorRGB() {
-        r = 0.0;
-        g = 0.0;
-        b = 0.0;
-    }
+    ColorRGB& operator+=(const ColorRGB& other);
+    ColorRGB operator+(const ColorRGB& other) const;
 
-    // Overloaded +=
-    ColorRGB& operator+=(const ColorRGB& other) {
-        r += other.r;
-        g += other.g;
-        b += other.b;
-        return *this;
-    }
+    ColorRGB& operator-=(const ColorRGB& other);
+    ColorRGB operator-(const ColorRGB& other) const;
 
-    // Overloaded + 
-    ColorRGB operator+(const ColorRGB& other) const {
-        return ColorRGB(r + other.r, g + other.g, b + other.b);
-    }
+    ColorRGB& operator*=(const ColorRGB& other);
+    ColorRGB operator*(const ColorRGB& other) const;
 
-    // Overloaded *= 
-    ColorRGB& operator*=(const ColorRGB& other) {
-        r *= other.r;
-        g *= other.g;
-        b *= other.b;
-        return *this;
-    }
+    ColorRGB& operator*=(double scalar);
+    ColorRGB operator*(double scalar) const;
 
-    // Overloaded *: ColorRGB * scalar
-    ColorRGB operator*(double scalar) const {
-        return ColorRGB(r * scalar, g * scalar, b * scalar);
-    }
+    ColorRGB& operator/=(double scalar);
+    ColorRGB operator/(double scalar) const;
 
-    // Overloaded /: ColorRGB / scalar
-    ColorRGB operator/(double scalar) const {
-        if (scalar != 0.0) {
-            return ColorRGB(r / scalar, g / scalar, b / scalar);
-        }
-        else {
-            // Handle division by zero or return a default color (e.g., black).
-            return ColorRGB();
-        }
-    }
-
-    // Overloaded *: scalar * ColorRGB
-    friend ColorRGB operator*(double scalar, const ColorRGB& color) {
-        return ColorRGB(color.r * scalar, color.g * scalar, color.b * scalar);
-    }
+    friend ColorRGB operator*(double scalar, const ColorRGB& color);
 };
