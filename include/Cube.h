f84629237f7707172e180f57019b5b9ee9453a78
rasmussvala <rassv453@student.liu.se>
2023-10-26 12:04:01 +0200

    separerat cube i h och cpp

--- include/Cube.h
+++ include/Cube.h
@@ -8,36 +8,26 @@
 
 class Cube {
 public:
-	Material getMaterial() const { return material; }
-	std::vector<Polygon*> getSurfaces() {
-		std::vector<Polygon*> surfaces;
-		surfaces.push_back(&surface1);
-		surfaces.push_back(&surface2);
-		surfaces.push_back(&surface3);
-		surfaces.push_back(&surface4);
-		surfaces.push_back(&surface5);
-		surfaces.push_back(&surface6);
-		return surfaces;
-	}
+    Cube(); // Declare the constructor
+    Material getMaterial() const;
+    std::vector<Polygon*> getSurfaces();
 private:
-	// DETTA SKA INTE VARA HÅRDKODAT I FRAMTIDEN 
-	ColorRGB color{ 0.0, 0.0, 0.4 };
+    ColorRGB color;
+    Material material;
 
-	Material material{ DIFFUSE, color };
+    glm::vec3 p0;
+    glm::vec3 p1;
+    glm::vec3 p2;
+    glm::vec3 p3;
+    glm::vec3 p4;
+    glm::vec3 p5;
+    glm::vec3 p6;
+    glm::vec3 p7;
 
-	glm::vec3 p0{ 6.0f, -1.0f, -5.0f };
-	glm::vec3 p1{ 9.0f, -1.0f, -5.0f };
-	glm::vec3 p2{ 9.0f, -4.0f, -5.0f };
-	glm::vec3 p3{ 6.0f, -4.0f, -5.0f };
-	glm::vec3 p4{ 6.0f, -1.0f, -2.0f };
-	glm::vec3 p5{ 9.0f, -1.0f, -2.0f };
-	glm::vec3 p6{ 9.0f, -4.0f, -2.0f };
-	glm::vec3 p7{ 6.0f, -4.0f, -2.0f };
-
-	Rectangle surface1{ p0, p1, p2, p3, material }; // botten
-	Rectangle surface2{ p4, p7, p6, p5, material }; // toppen
-	Rectangle surface3{ p0, p3, p7, p4, material }; // sidan mot kameran
-	Rectangle surface4{ p3, p2, p6, p7, material }; // sidan mot höger
-	Rectangle surface5{ p0, p4, p5, p1, material }; // sidan mot vänster
-	Rectangle surface6{ p1, p5, p6, p2, material }; // sidan mot bortre väggen
+    Rectangle surface1;
+    Rectangle surface2;
+    Rectangle surface3;
+    Rectangle surface4;
+    Rectangle surface5;
+    Rectangle surface6;
 };
--- src/Cube.cpp
+++ src/Cube.cpp
@@ -1,6 +1,39 @@
 #pragma once
 
 #include "../include/Cube.h"
-#include <vector>
 
+Material Cube::getMaterial() const {
+    return material;
+}
+
+std::vector<Polygon*> Cube::getSurfaces() {
+    std::vector<Polygon*> surfaces;
+    surfaces.push_back(&surface1);
+    surfaces.push_back(&surface2);
+    surfaces.push_back(&surface3);
+    surfaces.push_back(&surface4);
+    surfaces.push_back(&surface5);
+    surfaces.push_back(&surface6);
+    return surfaces;
+}
+
+// Initialize the member variables here
+Cube::Cube()
+    : color(0.0, 0.0, 0.4),
+    material(DIFFUSE, color),
+    p0(6.0f, -1.0f, -5.0f),
+    p1(9.0f, -1.0f, -5.0f),
+    p2(9.0f, -4.0f, -5.0f),
+    p3(6.0f, -4.0f, -5.0f),
+    p4(6.0f, -1.0f, -2.0f),
+    p5(9.0f, -1.0f, -2.0f),
+    p6(9.0f, -4.0f, -2.0f),
+    p7(6.0f, -4.0f, -2.0f),
+    surface1(p0, p1, p2, p3, material), // botten
+    surface2(p4, p7, p6, p5, material), // toppen
+    surface3(p0, p3, p7, p4, material), // sidan mot kameran
+    surface4(p3, p2, p6, p7, material), // sidan mot höger
+    surface5(p0, p4, p5, p1, material), // sidan mot vänster
+    surface6(p1, p5, p6, p2, material) // sidan mot bortre väggen
+{}
 
