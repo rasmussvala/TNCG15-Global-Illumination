29fc7890626f601de363746e5e83674c7f68e20e
Rasmus Svala <rasmus.svala@hotmail.com>
2024-12-14 19:05:53 +0100

    changed search to return vector of points

--- include/KDTree.h
+++ include/KDTree.h
@@ -1,7 +1,6 @@
 // C++ Program to Implement KD Tree: https://www.geeksforgeeks.org/kd-trees-in-cpp/
-// Modified to glm::vec3
+// Modified with glm::vec3 and range search
 #include <iostream>
-#include <array>
 #include <cmath>
 #include "../include/glm/glm.hpp"
 using namespace std;
@@ -46,27 +45,42 @@ private:
         return node;
     }
 
-    // Recursive function to search for a point in the KDTree
-    bool searchRecursive(Node *node, const glm::vec3 &point, float radius, int depth) const
+    // Recursive function to search for points in the KDTree within a given radius
+    vector<glm::vec3> searchRecursive(Node *node, const glm::vec3 &point, float radius, int depth)
     {
-        // Base case: If node is null, the point is not found
+        // Base case: If node is null, return empty vector
         if (node == nullptr)
-            return false;
+            return vector<glm::vec3>();
 
-        // If the current node is within range, return true
+        vector<glm::vec3> results;
+
+        // Check if current node's point is within the radius
         if (glm::distance(node->point, point) <= radius)
-            return true;
+            results.push_back(node->point);
 
         // Calculate current dimension (cd)
         int cd = depth % K;
 
-        // Compare point with current node and decide to go left or right
-        if (point[cd] < node->point[cd])
-            return searchRecursive(node->left, point, radius, depth + 1);
-        else
-            return searchRecursive(node->right, point, radius, depth + 1);
-    }
+        // Decide which subtree to explore first based on the current dimension
+        bool goLeft = point[cd] < node->point[cd];
+        Node *firstSubtree = goLeft ? node->left : node->right;
+        Node *secondSubtree = goLeft ? node->right : node->left;
 
+        // First, recursively search the closer subtree
+        vector<glm::vec3> firstResults = searchRecursive(firstSubtree, point, radius, depth + 1);
+        results.insert(results.end(), firstResults.begin(), firstResults.end());
+
+        // Check if we need to search the other subtree
+        // This happens when the splitting plane is closer than the radius
+        float planeDist = std::abs(point[cd] - node->point[cd]);
+        if (planeDist <= radius)
+        {
+            vector<glm::vec3> secondResults = searchRecursive(secondSubtree, point, radius, depth + 1);
+            results.insert(results.end(), secondResults.begin(), secondResults.end());
+        }
+
+        return results;
+    }
     // Recursive function to print the KDTree
     void printRecursive(Node *node, int depth) const
     {
@@ -102,7 +116,7 @@ public:
     }
 
     // Public function to search for a point in the KDTree
-    bool search(const glm::vec3 &point, float radius) const
+    vector<glm::vec3> search(const glm::vec3 &point, float radius)
     {
         return searchRecursive(root, point, radius, 0);
     }
